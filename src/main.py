c1e7b053b566b77ccbcc2dc6174582ac0bba42fd
Alex <alex.d.bondarev@gmail.com>
2021-09-19 18:58:35 +0100

    add logger

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
+from flask import request
+
 from src import app
 
 from src.endpoints import index  # noqa: E402, F401
+
+
+@app.after_request
+def log_request(response):
+    """Log request.
+
+    :param response:
+    :return:
+    """
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    host = request.host.split(':', 1)[0]
+
+    app.logger.info(f"method={request.method}, path={request.path}, "
+                    f"status={response.status_code}, "
+                    f"ip={ip}, host={host}, params={dict(request.args)}")
+
+    return response
+
 
 if __name__ == "__main__":
     app.run()
