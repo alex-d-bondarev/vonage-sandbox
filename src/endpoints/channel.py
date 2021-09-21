26e604a2dfd300bc80e202552ac4c55e29ee8397
Alex <alex.d.bondarev@gmail.com>
2021-09-21 10:47:42 +0100

    add better logs

--- src/endpoints/channel.py
+++ src/endpoints/channel.py
@@ -1,6 +1,8 @@
 """
 Index Page
 """
+from flask import request
+
 from src import app
 
 
@@ -9,4 +11,7 @@ def channel():
     """Default url
     :return:
     """
+    request_json = request.get_json()
+    app.logger.info(f"Received a message from {request_json['from']['type']} app "
+                    f"with text='{request_json['message']['content']['text']}'")
     return "Channel"
